fc8f0dd0faa1a35c0a3af047b0ef1970455bb2b6
DogeSWL <lusen.w1387@gmail.com>
2017-04-28 17:27:42 -0700

    completed caesar for crypto

--- lc101/crypto/caesar.py
+++ lc101/crypto/caesar.py
@@ -1,9 +1,38 @@
 def alphabet_position(letter):
+    # a-z -> 97-122
+    # A-Z -> 65-90
     asciiValue = ord(letter)
+
     return asciiValue
 
 def rotate_character(char, rot):
+    a = alphabet_position(char)
 
+    if char.isupper():
+        if a+rot > 90:
+            return chr((a+rot-90)+64)
+        else:
+            return chr(a+rot)
+    if char.islower():
+        if a+rot > 122:
+            return chr((a+rot-122)+96)
+        else:
+            return chr(a+rot)
+    else:
+        return chr(a)
 
+def encrypt(text, rot):
+    rtnText = ""
 
-print(alphabet_position("A"))
+    for i in text:
+        rtnText += rotate_character(i,rot)
+
+    return rtnText
+
+def main():
+    a = input("Type a message:")
+    b = input("Rotate by:")
+    print(encrypt(a,b))
+
+if __name__ == "__main__":
+    main()
